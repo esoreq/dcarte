c618bede3afe5d4c96b5fa5634794a3ff7b70a0c
Alex Capstick <alexcapstick97@gmail.com>
2022-10-19 09:43:38 +0100

    Adding kwargs argument to update functions
    This gives you access to the reload arguments from the update function.

--- dcarte/update.py
+++ dcarte/update.py
@@ -9,7 +9,21 @@ sep = os.sep
 cfg = get_config()
 NOW = date2iso(str(dt.datetime.now()))
 
-def update_raw():
+def update_raw(**kwargs):
+    """update_raw updates the raw data
+    stored locally.
+
+    To re-download all of the raw data, and overwrite the current
+    local datasets for that domain, you would use: 
+
+    >>> dcarte.update_raw(reload=True)
+
+    Args:
+        kwargs : Keyword arguments are passed to the dcarte.load function. update=True is already passed.
+
+    Returns:
+        None
+    """
     print("THIS FUNCTION MAY TAKE LONG")
     datasets = {}   
     files = list(Path(cfg['data']).rglob('*.parquet'))
@@ -17,11 +31,26 @@ def update_raw():
         datasets[i] = dict(dataset = file.stem,domain = file.parent.stem)
     datasets = pd.DataFrame(datasets).T
     for _,row in datasets.query('domain in ["raw","lookup"]').iterrows():
-        _ = load(row.dataset,row.domain,update=True)
+        _ = load(row.dataset,row.domain,update=True, **kwargs)
         
         
         
-def update_domain(domain ="base"):
+def update_domain(domain ="base", **kwargs):
+    """update_raw updates the raw data
+    stored locally.
+
+    To re-download all of the data for DOMAIN, and overwrite the current
+    local datasets for that domain, you would use: 
+
+    >>> dcarte.update_domain(domain=DOMAIN, reload=True)
+
+    Args:
+        domain (str): The name of the domain to update.
+        kwargs : Keyword arguments are passed to the dcarte.load function. update=True is already passed.
+
+    Returns:
+        None
+    """
     print("THIS FUNCTION MAY TAKE LONG")
     datasets = {}   
     files = list(Path(cfg['data']).rglob('*.parquet'))
@@ -29,4 +58,4 @@ def update_domain(domain ="base"):
         datasets[i] = dict(dataset = file.stem,domain = file.parent.stem)
     datasets = pd.DataFrame(datasets).T
     for _,row in datasets.query('domain in @domain').iterrows():
-        _ = load(row.dataset,row.domain,update=True)
+        _ = load(row.dataset,row.domain,update=True, **kwargs)
